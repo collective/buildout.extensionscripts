3ae868bddec37c8d2818db471011facbda694b20
Florian Schulze <florian.schulze@gmx.net>
2013-01-13 13:03:57 +0100

    Move readme.

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-import os
 from setuptools import setup
 
 setup(
     name='buildout.extensionscripts',
     description='Use simple python scripts as zc.buildout extensions',
-    long_description=open(os.path.join('buildout','extensionscripts','README.txt')).read(),
+    long_description=open('README.rst').read(),
     version='1.0',
     packages=['buildout', 'buildout.extensionscripts'],
     namespace_packages=['buildout'],
